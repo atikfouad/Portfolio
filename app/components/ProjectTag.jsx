2f6d41c721a860a393f43f61e5ecf3bbdc6176c3
atikfouad <atikfouad@hotmail.com>
2023-10-17 02:12:54 +0300

    tagfix

--- app/components/ProjectTag.jsx
+++ app/components/ProjectTag.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Projecttag = ({ onClick, name, isSelected }) => {
+const ProjectTag = ({ onClick, name, isSelected }) => {
   const buttonStyles = isSelected
     ? "text-white border-green-500"
     : "text-[#ADB7BE] border-slate-600 hover:border-white";
@@ -14,4 +14,4 @@ const Projecttag = ({ onClick, name, isSelected }) => {
   );
 };
 
-export default Projecttag;
+export default ProjectTag;
